1f02ac55ea7382a893d634020c0a522e08a60f45
saurav chamkure <sauravchamkure@gmail.com>
2026-07-23 18:11:22 +0530

    division of two integers

---
+++ Divioftwodigits.java
@@ -0,0 +1,11 @@
+package journeybegins;
+  public class Divioftwo
+  {
+    public static void main(String[] args)
+{
+  	int a = 8;
+  	int b = 7;
+  	int divi = a/b;
+    System.out.println("Division of two digits :"+divi);
+}
+}
