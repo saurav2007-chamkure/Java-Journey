ae8320f9e02d5f1838d82e6fe930d3b0c301d703
saurav chamkure <sauravchamkure@gmail.com>
2026-07-23 18:31:53 +0530

    Performing floating-point (decimal) division.

---
+++ Divioftwodigitss.java
@@ -0,0 +1,10 @@
+package journeybegins;
+  public class Divoftwodouble {
+	public static void main(String[] args)
+	{
+		int a = 34;
+		int b = 4;
+		double divi = (double) a/b;
+		System.out.println("Divison of two integers :"+divi);
+	}
+}
