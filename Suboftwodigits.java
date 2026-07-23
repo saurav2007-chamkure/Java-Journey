5d056cb4a5f8ca9ab76f642d2a1a8c4a7a609732
saurav chamkure <sauravchamkure@gmail.com>
2026-07-23 18:09:19 +0530

    Subtraction of two integers

---
+++ Suboftwodigits.java
@@ -0,0 +1,14 @@
+package journeybegins;
+
+public class Suboftwo 
+{
+  public static void main(String[] args) 
+	{
+		int a = 90;
+		int b = 30;
+		int sub = a-b;
+		
+		System.out.println("Subraction of two digits :" +sub);
+	}
+
+}
