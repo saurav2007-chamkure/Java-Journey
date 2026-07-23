5689d163899210916b89f1bf11c2b43b1a1c4499
saurav chamkure <sauravchamkure@gmail.com>
2026-07-23 18:14:29 +0530

    Multipliaction of two integers

---
+++ Multioftwodigits.java
@@ -0,0 +1,10 @@
+package journeybegins;
+    public class Multioftwo {
+    public static void main(String[] args)
+{
+   	int a = 25;
+   	int b = 5;
+	  int multi = a*b;
+   System.out.println("Division of two digits :"+multi);
+}
+}
