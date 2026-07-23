e29f2397f44c19ce8a4cb0cdaa0d286b8c8e12e6
saurav chamkure <sauravchamkure@gmail.com>
2026-07-23 18:07:20 +0530

    Addition of Two Integers

---
+++ Sumoftwodigits.java
@@ -0,0 +1,13 @@
+package journeybegins;
+public class Sumoftwo
+{
+  public static void main(String[] args)
+	{
+		int a = 30;
+		int b = 80;
+		int sum = a + b;
+		
+		System.out.println("Addition of two digits :"+sum);
+  }
+
+}
