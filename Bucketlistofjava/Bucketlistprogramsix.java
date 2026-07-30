bf50a1caa0d23bb73c321d85cd8a77131878b78a
saurav chamkure <sauravchamkure@gmail.com>
2026-07-30 18:52:08 +0530

    Implement SumDigits program to calculate digit sum
    Write a Java program that reads an integer and calculates the sum of its digits.

---
+++ Bucketlistofjava/Bucketlistprogramsix.java
@@ -0,0 +1,28 @@
+package journeybegins;
+
+import java.util.Scanner;
+
+public class SumDigits {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter an integer: ");
+        int num = sc.nextInt();
+
+        if (num < 0) {
+            num = -num;
+        }
+
+        int sum = 0;
+
+        for (; num != 0; num /= 10) {
+            sum += num % 10;
+        }
+
+        System.out.println("Sum of digits = " + sum);
+
+        sc.close();
+    }
+}
